8044e2bcff31d356681cd0d804f3a4461fa0d444
Pritam Das <pritam.das@brisksynergies.com>
2021-06-01 19:24:31 -0400

    Favicon, title updates

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -35,6 +35,11 @@ const CheckoutPage = ({cartItems, total}) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments
+            <br/>
+            424242424242 - 01/22 - CVV: 123
+        </div>
         <StripeCheckoutButton price={total} />
     </div>
 )
